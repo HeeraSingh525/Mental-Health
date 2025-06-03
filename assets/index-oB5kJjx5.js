49c5e9fd0f4eaf1a10f2804f70efea49c0efb819
heerasingh <heera5kwt@gamil.com>
2025-06-03 10:24:01 +0530

    Updates

---
+++ assets/index-oB5kJjx5.js
@@ -0,0 +1,1 @@
+import{j as t,S as e,O as r}from"./index-CPRzh8DC.js";const s=()=>t.jsx(e,{justifyContent:"center",alignItems:"center",minHeight:"100vh",bgcolor:"neutral.lighter",children:t.jsx(r,{})});export{s as default};
